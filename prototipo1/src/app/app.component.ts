34e4253b346c9fa077ad25e745e7023f572c46bc
Sergio Manrique Mendiola Arraez <smma2@gcloud.ua.es>
2024-11-21 19:10:41 +0100

    Versión con barra de encuesta y libreria instalada

--- prototipo1/src/app/app.component.ts
+++ prototipo1/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Marzipano from 'marzipano';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  public title: string = 'Incio';
+export class AppComponent implements OnInit {
+  public title: string = 'Inicio';
+
+  ngOnInit() {
+    // Initialize Marzipano
+    const viewerElement = document.querySelector('#pano');
+    const viewer = new Marzipano.Viewer(viewerElement);
+
+    // Create a source
+    const source = Marzipano.ImageUrlSource.fromString(
+      'https://www.marzipano.net/media/equirect/angra.jpg'
+    );
+
+    // Create a geometry
+    const geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
+
+    // Create a view
+    const limiter = Marzipano.RectilinearView.limit.traditional(1024, 100 * Math.PI / 180);
+    const view = new Marzipano.RectilinearView(null, limiter);
+
+    // Create a scene
+    const scene = viewer.createScene({
+      source: source,
+      geometry: geometry,
+      view: view,
+      pinFirstLevel: true
+    });
+
+    // Display the scene
+    scene.switchTo();
+  }
+  onChangeImage(direction: string) {
+    console.log(`Image change requested: ${direction}`);
+    // Implement the logic to change the image or perform other actions based on direction
+  }
 }
--- prototipo1/src/app/app.module.ts
+++ prototipo1/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonBarComponent } from './button-bar/button-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
