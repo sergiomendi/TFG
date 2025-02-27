c5d97a3760b8dccb9f6f02ace4442c0f01845485
Mendituber <smma2@gcloud.ua.es>
2025-02-27 14:03:35 +0100

    dependencias instaladas

--- frontend/prototipo1/src/app/app.component.ts
+++ frontend/prototipo1/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'prototipo1';
