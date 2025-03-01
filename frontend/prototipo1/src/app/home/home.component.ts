68e0939b79ce152c2894a230067993e985da5418
Sergio Manrique Mendiola Arraez <smma2@gcloud.ua.es>
2025-03-01 19:28:19 +0100

    ruta home

---
+++ frontend/prototipo1/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { MenuItem } from 'primeng/api';
+// import { Menubar } from 'primeng/menubar';
+// import { BadgeModule } from 'primeng/badge';
+// import { AvatarModule } from 'primeng/avatar';
+// import { InputTextModule } from 'primeng/inputtext';
+// import { CommonModule } from '@angular/common';
+// import { Ripple } from 'primeng/ripple';
+@Component({
+  selector: 'app-root',
+  imports: [
+    // Menubar,
+    // BadgeModule,
+    // AvatarModule,
+    // InputTextModule,
+    // Ripple,
+    // CommonModule,
+  ],
+
+  standalone: true,
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {}
