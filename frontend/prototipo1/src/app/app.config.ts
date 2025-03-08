9c41149154c096e4adef932b58842d7756546b70
Sergio Manrique Mendiola Arraez <smma2@gcloud.ua.es>
2025-03-08 18:08:43 +0100

    Update app.config.ts

--- frontend/prototipo1/src/app/app.config.ts
+++ frontend/prototipo1/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/Aura';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
