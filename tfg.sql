49de85fc957e54963824bd69c2fd36fbd4b1507f
Sergio Manrique Mendiola Arraez <smma2@gcloud.ua.es>
2025-05-03 11:25:17 +0200

    bd

--- tfg.sql
+++ tfg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2025 a las 16:35:44
+-- Tiempo de generación: 03-05-2025 a las 11:24:45
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `archivo`
+--
+
+CREATE TABLE `archivo` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `tipo` varchar(100) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `retos` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`retos`)),
+  `id_escena` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `archivo`
+--
+
+INSERT INTO `archivo` (`id`, `titulo`, `tipo`, `url`, `retos`, `id_escena`) VALUES
+(3, 'TFG BD (2).jpg', 'jpg', '3d634633-669b-4cb0-a4a2-17f07897381b.jpg', NULL, 58);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `escena`
 --
 
@@ -31,24 +53,21 @@ CREATE TABLE `escena` (
   `id` int(11) NOT NULL,
   `titulo` varchar(255) NOT NULL,
   `descripcion` varchar(255) DEFAULT NULL,
-  `fechaAlta` int(11) NOT NULL,
-  `fotos` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`fotos`))
+  `fechaAlta` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `escena`
 --
 
-INSERT INTO `escena` (`id`, `titulo`, `descripcion`, `fechaAlta`, `fotos`) VALUES
-(2, 'Cena familiar con múltiples platos', 'aaa', 1713133200, NULL),
-(3, 'Comedor escolar al mediodía', NULL, 1713392400, NULL),
-(4, 'Fiesta con buffet libree', NULL, 1713565200, NULL),
-(5, 'Playa en verano con bañadores', NULL, 1713738000, NULL),
-(6, 'Restaurante elegante con varios comensales', NULL, 1713910800, NULL),
-(7, 'Gimnasio con espejos', NULL, 1714083600, NULL),
-(8, 'Pasarela de moda', NULL, 1714256400, NULL),
-(13, 'Comedor escolar al mediodía', 'asdasd', 1713392400, NULL),
-(47, 'dasd', 'sadas', 1745676903, '[\"a76f49c9-964c-4aa8-82ad-6879413d385b.webp\"]');
+INSERT INTO `escena` (`id`, `titulo`, `descripcion`, `fechaAlta`) VALUES
+(2, 'Cena familiar con múltiples platos', 'aaa', 1713133200),
+(3, 'Comedor escolar al mediodía', NULL, 1713392400),
+(4, 'Fiesta con buffet libree', NULL, 1713565200),
+(5, 'Playa en verano con bañadores', NULL, 1713738000),
+(6, 'Restaurante elegante con varios comensales', NULL, 1713910800),
+(7, 'Gimnasio con espejos', NULL, 1714083600),
+(58, 'nueva tabla21', 'a', 1745774389);
 
 -- --------------------------------------------------------
 
@@ -72,7 +91,8 @@ CREATE TABLE `evento` (
 CREATE TABLE `experiencia` (
   `id` int(11) NOT NULL,
   `duracion` int(11) NOT NULL,
-  `fechaAlta` int(11) NOT NULL,
+  `fechaAlta` int(120) NOT NULL,
+  `fechaFin` int(120) DEFAULT NULL,
   `estresInicial` int(11) NOT NULL,
   `estresFinal` int(11) NOT NULL,
   `id_escena` int(11) NOT NULL,
@@ -83,9 +103,26 @@ CREATE TABLE `experiencia` (
 -- Volcado de datos para la tabla `experiencia`
 --
 
-INSERT INTO `experiencia` (`id`, `duracion`, `fechaAlta`, `estresInicial`, `estresFinal`, `id_escena`, `id_paciente`) VALUES
-(9, 0, 1745509434, 0, 0, 2, 4),
-(10, 0, 1745510000, 0, 0, 2, 4);
+INSERT INTO `experiencia` (`id`, `duracion`, `fechaAlta`, `fechaFin`, `estresInicial`, `estresFinal`, `id_escena`, `id_paciente`) VALUES
+(9, 0, 1745509434, NULL, 0, 0, 2, 4),
+(10, 0, 1745510000, NULL, 0, 0, 2, 4),
+(11, 0, 1746177124, NULL, 0, 0, 2, 4),
+(12, 0, 1746182187, NULL, 0, 0, 2, 4),
+(13, 0, 1746182215, NULL, 0, 0, 2, 5),
+(14, 0, 1746182266, NULL, 0, 0, 2, 4),
+(15, 0, 1746182413, NULL, 0, 0, 2, 5),
+(16, 0, 1746182471, NULL, 0, 0, 2, 5),
+(17, 0, 1746182500, NULL, 0, 0, 2, 5),
+(18, 0, 1746182534, NULL, 0, 0, 2, 4),
+(19, 0, 1746183526, NULL, 0, 0, 2, 5),
+(20, 0, 1746183613, NULL, 0, 0, 2, 5),
+(21, 0, 1746183642, 2147483647, 0, 0, 2, 5),
+(22, 2147483647, 1746183830, 2147483647, 0, 0, 2, 4),
+(23, 2147483647, 1746183944, 2147483647, 0, 0, 2, 5),
+(24, 2147483647, 1746184029, 2147483647, 0, 0, 2, 4),
+(25, 2147483647, 1746184086, 2147483647, 0, 0, 2, 5),
+(26, 2147483647, 1746184249, 2147483647, 0, 0, 2, 4),
+(27, 0, 1746184409, 1746184425, 0, 0, 2, 4);
 
 -- --------------------------------------------------------
 
@@ -105,11 +142,19 @@ CREATE TABLE `paciente` (
 --
 
 INSERT INTO `paciente` (`id`, `nombre`, `comentarios`, `fechaAlta`) VALUES
-(4, 'dasdas', 'asdsa', 1745332497);
+(4, 'dasdas', 'asdsa', 1745332497),
+(5, 'adasd', 'asdsadas', 1746177092);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `escena_fk` (`id_escena`);
 
 --
 -- Indices de la tabla `escena`
@@ -143,10 +188,16 @@ ALTER TABLE `paciente`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de la tabla `escena`
 --
 ALTER TABLE `escena`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT de la tabla `evento`
@@ -158,17 +209,23 @@ ALTER TABLE `evento`
 -- AUTO_INCREMENT de la tabla `experiencia`
 --
 ALTER TABLE `experiencia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `archivo`
+--
+ALTER TABLE `archivo`
+  ADD CONSTRAINT `escena_fk` FOREIGN KEY (`id_escena`) REFERENCES `escena` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `evento`
